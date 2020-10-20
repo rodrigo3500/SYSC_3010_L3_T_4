aa964635679c0d7be34a6b88064d6ae672616707
m4roon <tarunkalikivaya@cmail.carleton.ca>
2020-10-19 20:41:34 -0400

    :tada: B done :tada:

--- Lab5/AB.py
+++ Lab5/AB.py
@@ -1,9 +1,9 @@
 from A import *
+from B import *
 # SYSC3010 Lab 5 - AB.py
 #     The file to be jointly edited by Team Members A and B
 
 def function_AB():
-    function_A()
     print ("AB")
-
- 
+    function_A()
+    function_B()
