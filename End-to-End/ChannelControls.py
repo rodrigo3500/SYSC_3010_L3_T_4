993a8c3436bb4922b3692be4047818c2f37b4a92
rodrigo_3500 <rodrigo_3500@live.com>
2020-11-11 22:41:10 -0500

    adding functional test case 3 in test_1-5

--- End-to-End/ChannelControls.py
+++ End-to-End/ChannelControls.py
@@ -2,25 +2,29 @@ import requests
 import json
 import urllib.parse as urlencode
 
-w_key = "NHGIKFM0VW4TGHDA" # L3_T_4a2 write key
+w_key = "NHGIKFM0VW4TGHDA"  # L3_T_4a2 write key
+
 
 def clearChannel():
     try:
-        channel_id =1216791
+        channel_id = 1224300
         mydata = {}
-        mydata['api_key'] = '4X1V8UAUXRKAOI9F'
+        mydata['api_key'] = 'VF11C85OPY3Q5UR6'
         url = "http://api.thingspeak.com:80/channels/" + str(channel_id) + "/feeds.json"
         jsondata = json.dumps(mydata)
-        r = requests.delete(url,params=mydata)
+        r = requests.delete(url, params=mydata)
         print(r.status_code)
     except Exception as e:
-        print (e)
+        print(e)
+
 
 def uploadPersonEnterExitRoomData(action, roomId, occupants, time):
     return uploadData(action, roomId, occupants, time)
 
+
 def uploadMaxCapChangeData(action, roomId, newMax, time):
     return uploadData(action, roomId, newMax, time)
+
 
 def uploadMaxCapReachedData(action, roomId, time):
     return uploadData(action, roomId, "", time)
@@ -28,8 +32,8 @@ def uploadMaxCapReachedData(action, roomId, time):
 
 def uploadData(field1, field2, field3, field4):
     try:
-        params = {'field1': field1, 'field2':field2, 'field3':field3, 'field4':field3, 'key':w_key}
-        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        params = {'field1': field1, 'field2': field2, 'field3': field3, 'field4': field3, 'key': w_key}
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded", "Accept": "text/plain"}
         url = "http://api.thingspeak.com:80/update"
         r = requests.post(url, params=params, headers=headers)
         return r.status_code
--- End-to-End/test_1-5.py
+++ End-to-End/test_1-5.py
@@ -1,46 +1,15 @@
 import unittest
-import http.client as httplib
-from urllib.parse import urlencode
+import datetime
 
-from ChannelControls import clearChannel
+from ChannelControls import *
 
 w_key = "NHGIKFM0VW4TGHDA"  # L3_T_4a2 write key
 r_key = "1IF97D5OLYHPX0ER"  # L3_T_4a2 read key
 
 
-# Test 1 - Simulate a person entering a room
-def person_entered_room():
-    # Send person entered room data to thingspeak
-    print("test")
-
-
-# Test 2 - Simulate a person exiting a room
-def person_exited_room():
-    # Send person exited room data to thingspeak
-    print("test")
-
-
-# Test 3 - Simulate the max capacity being increased
-def max_capacity_increased():
-    # Send max capacity increased data to thingspeak
-    print("test")
-
-
-# Test 4 - Simulate the max capacity being decreased
-def max_capacity_decreased():
-    # Send max capacity decreased data to thingspeak
-    print("test")
-
-
-# Test 5 - Simulate the max capacity being reached
-def max_capacity_reached():
-    # Send person exited room data to thingspeak
-    print("test")
-
-
 # Tests one instances of all basic communication messages in the system
-class BasicCommunication:
-
+class BasicCommunication(unittest.TestCase):
+    '''
     # Test 1 - Simulate a person entering a room
     def test_person_entered_room(self):
         # Delete all entries in ThinkSpeak
@@ -56,27 +25,33 @@ class BasicCommunication:
 
         # Run test # 2
         person_exited_room()
+    '''
 
     # Test 3 - Simulate the max capacity being increased
-    def test_max_capacity_increased():
+    def test_max_capacity_increased(self):
         # Delete all entries in ThinkSpeak
         clearChannel()
 
         # Run test # 3
-        max_capacity_increased
+        self.assertEqual(uploadMaxCapChangeData("max_capacity_increased", 3, 10, datetime.datetime.now),200)
 
+
+    '''
     # Test 4 - Simulate the max capacity being decreased
-    def test_max_capacity_decreased():
+    def test_max_capacity_decreased(self):
         # Delete all entries in ThinkSpeak
         clearChannel()
 
         # Run test # 4
-        max_capacity_decreased
+        uploadMaxCapChangeData(action, roomId, newMax, time)
 
     # Test 5 - Simulate the max capacity being reached
-    def test_max_capacity_reached():
+    def test_max_capacity_reached(self):
         # Delete all entries in ThinkSpeak
         clearChannel()
 
         # Run test # 5
-        max_capacity_reached()
+        uploadMaxCapReachedData(action, roomId, time)
+    '''
+if __name__ == '__main__':
+    unittest.main()
