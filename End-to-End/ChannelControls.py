96bb4c7faa95e78741cd0d4f9625001e6e83c301
Hari Govind <grhari1998@gmail.com>
2020-11-11 21:15:11 -0500

    Test: testing get all data points from thingspeak and x number of datapoints

--- End-to-End/ChannelControls.py
+++ End-to-End/ChannelControls.py
@@ -3,32 +3,34 @@ import json
 import urllib.parse as urlencode
 
 w_key = "NHGIKFM0VW4TGHDA" # L3_T_4a2 write key
+#w_key = "R2E49ELP96PB9RCX" 
+
 
 def clearChannel():
     try:
-        channel_id =1216791
+        channel_id =1224300
         mydata = {}
-        mydata['api_key'] = '4X1V8UAUXRKAOI9F'
+        mydata['api_key'] = 'VF11C85OPY3Q5UR6'
         url = "http://api.thingspeak.com:80/channels/" + str(channel_id) + "/feeds.json"
         jsondata = json.dumps(mydata)
         r = requests.delete(url,params=mydata)
-        print(r.status_code)
+        return r.status_code
     except Exception as e:
         print (e)
 
 def uploadPersonEnterExitRoomData(action, roomId, occupants, time):
-    return uploadData(action, roomId, occupants, time)
+    return uploadData(action, roomId, occupants, time.__str__())
 
 def uploadMaxCapChangeData(action, roomId, newMax, time):
-    return uploadData(action, roomId, newMax, time)
+    return uploadData(action, roomId, newMax, time.__str__())
 
 def uploadMaxCapReachedData(action, roomId, time):
-    return uploadData(action, roomId, "", time)
+    return uploadData(action, roomId, "", time.__str__())
 
 
 def uploadData(field1, field2, field3, field4):
     try:
-        params = {'field1': field1, 'field2':field2, 'field3':field3, 'field4':field3, 'key':w_key}
+        params = {'field1': field1, 'field2':field2, 'field3':field3, 'field4':field4, 'key':w_key}
         headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
         url = "http://api.thingspeak.com:80/update"
         r = requests.post(url, params=params, headers=headers)
--- End-to-End/test_6-8.py
+++ End-to-End/test_6-8.py
@@ -1,36 +1,51 @@
 import unittest
-import http.client as httplib
-from urllib.parse import urlencode
 import ChannelControls
 import datetime
+import requests
+import json
+import time
 
 w_key = "NHGIKFM0VW4TGHDA"  # L3_T_4a2 write key
 r_key = "1IF97D5OLYHPX0ER"  # L3_T_4a2 read key
+URL = 'https://api.thingspeak.com/channels/1224300/feeds.json?api_key='   
 
 # Sends sample data log entries to ThingSpeak.
 class TestDataRetrival(unittest.TestCase):
-
     def setUp(self):
-        ChannelControls.clearChannel()
-
-    # Test #
-    # Describe test functions here
-    def test_testname(self): # Sample test definition, to be updated
+        # Given that the only the test data is present in the thingspeak channel
+        clearResponse= ChannelControls.clearChannel()
+        self.assertEqual(200, clearResponse)
         uploadEnter = ChannelControls.uploadPersonEnterExitRoomData("enter", 1, 2, datetime.datetime.now())
         self.assertEqual(200,uploadEnter)
-        
-        uploadExit = ChannelControls.uploadPersonEnterExitRoomData("exit", 1, 1, datetime.datetime.now())
+        time.sleep(5)
+        uploadExit = ChannelControls.uploadPersonEnterExitRoomData("exit", 3, 1, datetime.datetime.now())
         self.assertEqual(200,uploadExit)
-        
-        uploadMaxIncrease = ChannelControls.uploadMaxCapChangeData("max capacity increased",1,10, datetime.datetime.now())
+        time.sleep(5)
+        uploadMaxIncrease = ChannelControls.uploadMaxCapChangeData("max_capacity_increased",1,10, datetime.datetime.now())
         self.assertEqual(200,uploadMaxIncrease)
-
-        uploadMaxDecrease = ChannelControls.uploadMaxCapChangeData("max capacity decreased",1,9, datetime.datetime.now())
+        time.sleep(5)
+        uploadMaxDecrease = ChannelControls.uploadMaxCapChangeData("max_capacity_decreased",3,9, datetime.datetime.now())
         self.assertEqual(200,uploadMaxDecrease)
-
-        uploadMaxReached = ChannelControls.uploadMaxCapReachedData("max capacity reached", 1, datetime.datetime.now())
+        time.sleep(5)
+        uploadMaxReached = ChannelControls.uploadMaxCapReachedData("max_capacity_reached", 3, datetime.datetime.now())
         self.assertEqual(200,uploadMaxReached)
 
+
+    # Test #
+    # Asserting to ensure all uploaded data can be retreived. The setup function uploads 5 data points for testing
+    def test_getAllData(self): 
+        fullURL = URL+r_key
+        getData = requests.get(fullURL).json()
+        entries = getData['feeds']
+        self.assertEqual(5,len(entries))
+
+    # Testing to ensure that just the last three data points can be retreived
+    def test_getSpecificEntries(self):
+        HEADER = '&results=3'
+        fullURL = URL+r_key+HEADER
+        getData = requests.get(fullURL).json()
+        entries = getData['feeds']
+        self.assertEqual(3,len(entries))
 
 if __name__=="__main__":
     unittest.main()
