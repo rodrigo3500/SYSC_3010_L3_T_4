18f3c28988bcab839cc2695e730c548172e843af
Hari Govind <grhari1998@gmail.com>
2020-11-30 23:55:31 -0500

    feat: updated to use hw interrupts instead of polling

--- endNodes/buttonControl.py
+++ endNodes/buttonControl.py
@@ -1,4 +1,7 @@
+# Author: Hari
+# Setup input buttons and uses interrupts to trigger actions inside program
 import RPi.GPIO as GPIO
+import capacityControl as capacityControl
 
 increaseCapacity = 1
 decreaseCapacity = 2
@@ -9,8 +12,13 @@ def setupButtons():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(16,GPIO.IN)
     GPIO.setup(18,GPIO.IN)
+    GPIO.add_event_detect(16, GPIO.RISING)
+    GPIO.add_event_callback(16, callIncreaseCap)
+    GPIO.add_event_detect(18, GPIO.RISING)
+    GPIO.add_event_callback(18, callDecreaseCap)
 
 # Read if the up button is pressed or not
+# DEPRICATED
 def readUpButton():
     if(GPIO.input(16)==GPIO.HIGH):
         return True
@@ -18,6 +26,7 @@ def readUpButton():
         return False
 
 # Read if the down button is pressed or not
+# DEPRICATED 
 def readDownButton():
     if(GPIO.input(18)==GPIO.HIGH):
         return True
@@ -25,6 +34,7 @@ def readDownButton():
         return False
 
 # Determine if the user wants to increase of decrement the capacity
+# DEPRICATED
 def readButtonInputs():
     upPressed = readUpButton()
     downPressed = readDownButton()
@@ -33,3 +43,12 @@ def readButtonInputs():
     elif(downPressed and (not upPressed)):
         return decreaseCapacity
     return noChange
+
+# callback function to update values when incremented
+def callIncreaseCap(callBack):
+    capacityControl.increaseCapacity()
+
+# callback function to update values when decremented
+def callDecreaseCap(callBack):
+    capacityControl.decreaseCapacity()
+
