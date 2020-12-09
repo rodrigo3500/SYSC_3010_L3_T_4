4122b89aee5e315ec19f64620f9b0a96cdbb5ff9
harigovind1998 <grhari1998@gmail.com>
2020-12-08 22:12:12 -0500

    test: updated commented. Moved starting delay into separate function

--- Demo2/pushButtonTest.py
+++ Demo2/pushButtonTest.py
@@ -1,26 +1,30 @@
+# Author: Hari
 import RPi.GPIO as GPIO
 import unittest
 import time
 
+# Setup button as GPIO inputs. Ports are predesignated in the design doc.
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(16,GPIO.IN)
     GPIO.setup(18,GPIO.IN)
 
 class TestPushButtons(unittest.TestCase):
+    
+    # Setting a 5 second delay between prompt and start of test to give user adequate time to press or unpress the correct button
+    def printDelay():
+        print("Test starting in 5...")
+        x = 4
+        while (x > 0):
+            time.sleep(1)
+            print("{}...".format(x))
+            x = x -1
+        print("starting...")
+    
+    # Test for up button unpressed, required to test pull down resistor
     def test_upButtonUnpressed(self):
         print("Please do not touch any of the buttons")
-        print("Test starting in 5...")
-        time.sleep(1)
-        print("4..")
-        time.sleep(1)
-        print("3...")
-        time.sleep(1)
-        print("2...")
-        time.sleep(1)
-        print("1...")
-        time.sleep(1)
-        print("starting...")
+        printDelay()
         secondsPassed = 0
         buttonNotPressed = 0
         while(secondsPassed <5):
@@ -30,20 +34,10 @@ class TestPushButtons(unittest.TestCase):
             secondsPassed = secondsPassed + 1
         self.assertEqual(5,buttonNotPressed)
         print()
-
+    # Test for up button pressed
     def test_upButtonPressed(self):
         print("Please press the up button (RED cap), and hold until prompted to let go of the button")
-        print("Test starting in 5...")
-        time.sleep(1)
-        print("4..")
-        time.sleep(1)
-        print("3...")
-        time.sleep(1)
-        print("2...")
-        time.sleep(1)
-        print("1...")
-        time.sleep(1)
-        print("Starting...")
+        printDelay()
         secondsPassed = 0
         buttonPressed = 0
         while(secondsPassed < 5):
@@ -54,20 +48,11 @@ class TestPushButtons(unittest.TestCase):
         print("Please let go of the button.")
         self.assertEqual(5,buttonPressed)
         print()
-
+    
+    # Test for down button unpressed, required to test pull down resistor
     def test_downButtonUnpressed(self):
         print("Please do not touch any of the buttons")
-        print("Test starting in 5...")
-        time.sleep(1)
-        print("4..")
-        time.sleep(1)
-        print("3...")
-        time.sleep(1)
-        print("2...")
-        time.sleep(1)
-        print("1...")
-        time.sleep(1)
-        print("starting...")
+        printDelay()
         secondsPassed = 0
         buttonNotPressed = 0
         while(secondsPassed <5):
@@ -78,19 +63,10 @@ class TestPushButtons(unittest.TestCase):
         self.assertEqual(5,buttonNotPressed)
         print()
 
+    # Test for down button pressed
     def test_downButtonPressed(self):
         print("Please press the down button (YELLOW cap), and hold until prompted to let go of the button")
-        print("Test starting in 5...")
-        time.sleep(1)
-        print("4..")
-        time.sleep(1)
-        print("3...")
-        time.sleep(1)
-        print("2...")
-        time.sleep(1)
-        print("1...")
-        time.sleep(1)
-        print("Starting...")
+        printDelay()
         secondsPassed = 0
         buttonPressed = 0
         while(secondsPassed < 5):
